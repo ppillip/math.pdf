8445910bc6c3411be99041d81afe29c1f6b6d351
ppillip <ppillip@gmail.com>
2012-09-27 23:30:33 +0900

    화일폴더 추가

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  app.use(express.cookieParser('nice'));
+  app.use(express.session({ secret: 'ppillip' }));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -28,10 +28,17 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
+app.get('*',function(req,res,next){
+    console.log('무조건 진입');
+    next(); //넥스트로 빠져 나감
+});
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/user', user.greeting);
+app.get('/user/login/:id', user.login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,22 @@
-
 /*
  * GET users listing.
  */
 
 exports.list = function(req, res){
   res.send("respond with a resource");
+};
+
+exports.login = function(req, res){
+  req.session.userid = req.params.id;
+  console.log(req.session.userid);
+  res.send("hi "+ req.params.id + "~!");
+};
+
+exports.greeting = function(req, res){
+  console.log("current user : " + req.session.userid);
+  if(!!req.session.userid){
+    res.send("hi "+ req.session.userid + "~! nice to see you~! is this your first time ?");
+  }else{
+    res.send("who are you?");
+  }
 };
